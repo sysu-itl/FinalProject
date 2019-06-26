0d523fd6b984c98120b4be2a1732851fcab3a0d9
ddghost <544011312@qq.com>
2019-06-26 17:29:09 +0800

    update

---
+++ code/Classes/MainScene.cpp
@@ -0,0 +1,115 @@
+﻿#include "MainScene.h"
+#include "MenuScene.h"
+#include "SimpleAudioEngine.h"
+#include "math\MathUtil.h"
+#include "cocos2d.h"
+
+#include "propsFactory.h"
+#include "BlockCreator.h"
+#include "Hint.h"
+#include "Music.h"
+#include "Database.h"
+
+
+
+USING_NS_CC;
+
+void MainScene::setPhysicsWorld(PhysicsWorld* world) 
+{ 
+	m_physicWorld = world; 
+}
+
+Scene* MainScene::createScene()
+{
+	srand((unsigned)time(NULL));
+	auto scene = Scene::createWithPhysics();
+
+	scene->getPhysicsWorld()->setAutoStep(true);
+
+	// Debug 模式
+	if (DEBUG) {
+		scene->getPhysicsWorld()->setDebugDrawMask(PhysicsWorld::DEBUGDRAW_ALL);
+		Test::getInstance()->OpenTestMode();
+	}
+	auto layer = MainScene::create();
+	//设置物理世界
+	layer->setPhysicsWorld(scene->getPhysicsWorld());
+
+	scene->addChild(layer);
+	scene->getPhysicsWorld()->setGravity(Vec2(0, -980));
+	layer->setJoint();
+	propsFactory::getInstance()->setLayer(layer);
+	propsFactory::getInstance()->setPhysicsWorld(scene->getPhysicsWorld());
+	return scene;
+}
+
+
+static void problemLoading(const char* filename)
+{
+	
+	printf("Error while loading: %s\n", filename);
+	printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in MainSceneScene.cpp\n");
+}
+
+
+bool MainScene::init()
+{
+
+	if (!Layer::init())
+	{
+		return false;
+	}
+	//设置初始变量
+	visibleSize = Director::getInstance()->getVisibleSize();
+	origin = Director::getInstance()->getVisibleOrigin();
+	canJump = 0;
+
+	jumpCount = Database::getInstance()->getPlayerJumpCount(0);
+	life = Database::getInstance()->getPlayerLife(0);
+	highScore = Database::getInstance()->getPlayerHighScore(0);
+	Money = Database::getInstance()->getPlayerCurrentScore(0);
+	Database::getInstance()->setNewRecord(false);
+
+	//创建角色
+	createPlayer();
+	//创建鞋子的条
+	createRestTimeBar();
+	//初始化地图
+	initMap();
+	//创建背景
+	createBackground();
+	//添加监听器
+	addListener();
+
+	schedule(schedule_selector(MainScene::updateMap), 0.02f, kRepeatForever, 0.0f);
+	schedule(schedule_selector(MainScene::StopAction), 0.01f, kRepeatForever, 0.0f);
+	schedule(schedule_selector(MainScene::speedUp), 15.0f, kRepeatForever, 0.0f);
+
+	//创建当前分数的label
+	currentScore = Label::create("Score: 0", "Arial", 20);
+	currentScore->setPosition(visibleSize.width - 100, visibleSize.height - 100);
+	currentScore->setColor(Color3B::BLACK);
+	addChild(currentScore, 3);
+	//创建最高分的lebel
+	MostHigh = Label::create("High Score: 0", "Arial", 20);
+	MostHigh->setPosition(visibleSize.width - 100, visibleSize.height - 50);
+	MostHigh->setColor(Color3B::BLACK);
+	addChild(MostHigh, 3);
+
+	//将最高分和分数具体值展示
+	updateScoreLabel();
+	updateHighScoreLabel();
+
+	Music::getInstance()->playBackgroundMusic();
+	return true;
+}
+
+//创建背景
+void MainScene::createBackground() 
+{
+	Sprite* background = Sprite::create("background.png");
+	background->setContentSize(visibleSize);
+	background->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2));
+	this->addChild(background, -1);
+}
+
---
+++ code/Classes/MainScene.h
@@ -0,0 +1,105 @@
+﻿#ifndef __HELLOWORLD_SCENE_H__
+#define __HELLOWORLD_SCENE_H__
+
+#include "cocos2d.h"
+#include "BlockCreator.h"
+#include "Test.h"
+USING_NS_CC;
+using namespace std;
+
+class MainScene : public cocos2d::Layer
+{
+public:
+	void setPhysicsWorld(PhysicsWorld * world);
+	static cocos2d::Scene* createScene();
+
+	virtual bool init();
+
+	void scheduleRestTime(float dt);
+
+	void createPlayer();
+
+	void createBackground();
+
+	void createRestTimeBar();
+
+	void setJoint();
+
+	void addListener();
+
+	void removeListener();
+
+	void explosion(Vec2 pos, float radius);
+
+	bool onContactBegin(PhysicsContact & contact);
+
+	bool onContactEnd(PhysicsContact & contact);
+
+	bool pairMatch(int a1, int a2, int b1, int b2);
+
+	void onKeyPressed(EventKeyboard::KeyCode code, Event * event);
+
+	void onKeyReleased(EventKeyboard::KeyCode code, Event * event);
+
+	void updateMap(float dt);
+
+	void StopAction(float dt);
+
+	void updateScoreLabel();
+
+	int getCurrentScore();
+
+	void updateHighScoreLabel();
+	//
+	void initMap();
+
+	void contactGold();
+	
+	void contactShoe();
+
+	void contactMedicine();
+	
+	void speedUp(float dt);
+
+	void changeSpeed(double multi);
+
+	void resetShoePosibilityRange(float dt);
+	
+	void resetMedicinePosibilityRange(float dt);
+	// implement the "static create()" method manually
+	CREATE_FUNC(MainScene);
+private:
+	PhysicsWorld * m_physicWorld;
+	Layer* worldLayer;
+
+	Vec2 origin;
+	Size visibleSize;
+	float brickSize = 13;
+	int canJump;
+	bool isStop = false;
+	float playerleftOfRightVel = 500;
+	Sprite* player;
+	std::list<Sprite*> blockList;
+	cocos2d::Vector<SpriteFrame*> playerRun;
+	float playerVelocity;
+	const float	maxSpeed = 200,
+				minSpeed = 800;
+
+	int jumpCount;
+	int Money;
+	int highScore;
+	int life;
+	int difficulty = 0;
+	float shoeTime = 15;
+
+
+	// 标签
+	Label* blocknum;
+	Label* MostHigh;
+	Label* currentScore;
+	ProgressTimer* restTimeProgressTimer;
+};
+
+
+
+#endif // __HELLOWORLD_SCENE_H__

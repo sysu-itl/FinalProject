311f273148e665699c1c2ada0a9d68c2e75a6f9d
emilyblues <liaolei3@mail2.sysu.edu.cn>
2019-06-26 11:08:44 +0800

    add game resources, include bgm, fonts, background, sqlite

---
+++ Resources/background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="background" tilewidth="64" tileheight="45" tilecount="252" columns="18">
+ <image source="background.png" width="1167" height="655"/>
+</tileset>

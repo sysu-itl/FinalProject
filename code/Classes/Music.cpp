dd91a68e22bbe4db4d97bd20b55d480f0addbd2c
ZTao-z <1124693098@qq.com>
2019-06-26 16:33:45 +0800

    Update Music.cpp

--- code/Classes/Music.cpp
+++ code/Classes/Music.cpp
@@ -1,99 +1,77 @@
 #include "Music.h"
+#include "SimpleAudioEngine.h"
 
 Music* Music::instance = nullptr;
 
-Music* Music::getInstance() 
-{
+Music* Music::getInstance() {
 	if (instance == nullptr) {
 		instance = new Music();
 	}
 	return instance;
 }
 
-
-Music::Music() 
-{
-	
+Music::Music() {
+	this->volumn = 1.0f;
 }
 
-void Music::preloadMusic() 
-{
-	experimental::AudioEngine::lazyInit();
-	
-	experimental::AudioEngine::preload("music/bgm1.mp3");
-	experimental::AudioEngine::preload("music/die.mp3");
-	experimental::AudioEngine::preload("music/jump.mp3");
-	experimental::AudioEngine::preload("music/eatgold.mp3");
-	experimental::AudioEngine::preload("music/eatshoe.mp3");
-
-	Dictionary* settingDic = SettingScene::getSettingDict();
-	int MusicVolumnPercentage = settingDic->valueForKey("MusicVolumn")->intValue();
-	int EffectVolumnPercentage = settingDic->valueForKey("EffectVolumn")->intValue();
-	MusicVolumn = MusicVolumnPercentage / 100.0;
-	EffectVolumn = EffectVolumnPercentage / 100.0;
-
-	backgroudSongId = experimental::AudioEngine::INVALID_AUDIO_ID;
-	effectSongId = experimental::AudioEngine::INVALID_AUDIO_ID;
-
+void Music::preloadMusic() {
+	auto audio = SimpleAudioEngine::getInstance();
+	audio->preloadBackgroundMusic("music/bgm1.mp3");
+	audio->preloadEffect("music/die.mp3");
+	audio->preloadEffect("music/jump.mp3");
+	audio->preloadEffect("music/eatgold.mp3");
+	audio->preloadEffect("music/eatshoe.mp3");
+	audio->setBackgroundMusicVolume(this->volumn);
+	audio->setEffectsVolume(this->volumn/2);
 }
 
 // 0: die, 1: jump, 2: eat gold, 3: eat shoe 
-void Music::playMusic(int mode) 
-{
+void Music::playMusic(int mode) {
+
 	switch (mode)
 	{
 	case 0:
-		this->effectSongId = experimental::AudioEngine::play2d("music/die.mp3");
+		this->songId = SimpleAudioEngine::getInstance()->playEffect("music/die.mp3");
 		break;
 	case 1:
-		this->effectSongId = experimental::AudioEngine::play2d("music/jump.mp3");
+		this->songId = SimpleAudioEngine::getInstance()->playEffect("music/jump.mp3");
 		break;
 	case 2:
-		this->effectSongId = experimental::AudioEngine::play2d("music/eatgold.mp3");
+		this->songId = SimpleAudioEngine::getInstance()->playEffect("music/eatgold.mp3");
 		break;
 	case 3:
-		this->effectSongId = experimental::AudioEngine::play2d("music/eatshoe.mp3");
+		this->songId = SimpleAudioEngine::getInstance()->playEffect("music/eatshoe.mp3");
 		break;
 	default:
 		break;
 	}
-	setEffectVolume(EffectVolumn);
 }
 
-void Music::setEffectVolume(float volumn) 
-{
-	EffectVolumn = volumn ;
-	experimental::AudioEngine::setVolume(effectSongId, EffectVolumn);
+void Music::stopCurrentEffectMusic() {
+	SimpleAudioEngine::getInstance()->stopEffect(this->songId);
+	this->songId = -1;
+}
+void Music::stopAllEffectMusic() {
+	SimpleAudioEngine::getInstance()->stopAllEffects();
+	this->songId = -1;
 }
 
-void Music::setBackgroundVolume(float volumn)
-{
-	MusicVolumn = volumn;
-	experimental::AudioEngine::setVolume(backgroudSongId, MusicVolumn);
+void Music::pauseCurrentEffectMusic() {
+	SimpleAudioEngine::getInstance()->pauseEffect(this->songId);
 }
 
-void Music::stopCurrentEffectMusic() 
-{
-	experimental::AudioEngine::stop(this->effectSongId);
-	this->effectSongId = experimental::AudioEngine::INVALID_AUDIO_ID;
+void Music::pauseAllEffectMusic() {
+	SimpleAudioEngine::getInstance()->pauseAllEffects();
 }
 
-
-
-void Music::playBackgroundMusic() 
-{
-	backgroudSongId = experimental::AudioEngine::play2d("music/bgm1.mp3", true);
-	setBackgroundVolume(MusicVolumn );
+void Music::playBackgroundMusic() {
+	SimpleAudioEngine::getInstance()->playBackgroundMusic("music/bgm1.mp3", true);
 }
 
-void Music::pauseBackgroundMusic() 
-{
-	experimental::AudioEngine::pause(backgroudSongId);
-	
+void Music::pauseBackgroundMusic() {
+	SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
 }
 
-void Music::stopBackgroundMusic() 
-{
-	experimental::AudioEngine::stop(backgroudSongId);
-	backgroudSongId = experimental::AudioEngine::INVALID_AUDIO_ID;
+void Music::stopBackgroundMusic() {
+	SimpleAudioEngine::getInstance()->stopBackgroundMusic();
 }
